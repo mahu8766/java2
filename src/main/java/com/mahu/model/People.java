41b6c2dcd8d822c2e8dfb3bfc1bfe08fb4e7e541
mahu8766@hotmail.com <mahu8766@hotmail.com>
2018-05-24 16:25:00 +0800

    add  java

--- src/main/java/com/mahu/model/People.java
+++ src/main/java/com/mahu/model/People.java
@@ -1,13 +1,9 @@
 package com.mahu.model;
 
-
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name="people")
